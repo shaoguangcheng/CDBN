eae627592d357b220716df05e2fd0051f5add192
程少光 <chengshaoguang1291@gmail.com>
2014-11-02 00:03:47 +0800

    add class crbm

--- matlab_v2/cdbn/util/sigmoid.m
+++ matlab_v2/cdbn/util/sigmoid.m
@@ -1,5 +1,5 @@
 function [ y ] = sigmoid(x)
 % f(x) = 1/(1+exp(-x))
-y = 1./(1 + exp(-x));
-end
+%y = 1./(1 + exp(-x));
+y = bsxfun(@rdivide, 1, (1+exp(-x)));
 
--- matlab_v2/cdbn/util/softmax.m
+++ matlab_v2/cdbn/util/softmax.m
@@ -1,7 +1,12 @@
-function [ output_args ] = softmax()
-%UNTITLED5 此处显示有关此函数的摘要
-%   此处显示详细说明
+function [ y ] = softmax(x, dim)
+% y = softmax(x, [dim])
+% Compute softmax maxtrix along dimision dim. Default dim is 1.
 
-
+if notDefined('dim')
+    dim = 1;
 end
+
+maxX = max(x, [], dim);
+tmp = exp(bsxfun(@minus, x, maxX));
+y = bsxfun(@rdivide, tmp, sum(tmp, sum));
 
